6bb8dcc6ae947b00af8aedc5174b678fb9638098
Hihax <35403419+Hihax@users.noreply.github.com>
2019-06-15 19:07:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,15 +14,33 @@ bot.on("message", function (message) {
     if(message.member.hasPermission("ADMINISTRATOR")) return message.reply('Bonjour maître');
     message.reply('Bonjour')
     }
+    
     if(message.content === 'P!sheets') {
     message.reply("Voici l'URL : https://docs.google.com/spreadsheets/d/1dLyP47y2vrVqWxMgKBA6lpN1v18QJ4xgWOSpSHWj0KQ/edit#gid=1860824155")
     }
+    
     if(message.content === 'P!mail') {
     message.reply("Voici l'adresse mail : zeakatsuki@gmail.com")
     }
+    
     if(message.content === 'P!password') {
     message.reply("Voici le mot de passe du compte google : 5z*R9g97kW")
     }
+    
+        if(message.content === 'P!co') {
+    message.reply(" ")
+    var sender = message.content
+    message.delete
+    logs.send("@everyone " sender "s'est connecté")
+    }
+    
+        if(message.content === 'P!deco') {
+    message.reply(" ")
+    var sender = message.content
+    message.delete
+    logs.send("@everyone " sender "s'est déconnecté")
+    }
+    
     if(message.content === 'P!help') {
     message.reply("Voici la liste des commandes : ")
     message.reply("**P!sheet** : affiche la liste des coordonnées du pala")
